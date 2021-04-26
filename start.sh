b33e01a93ad8928c7b02d6119f8b9f5c2dff688c
Laurent Bovet <laurent.bovet@windmaster.ch>
2021-04-26 23:32:27 +0200

    New orac main. Pattern Gain module. Shake it.

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-akai=20
+akai=$(aconnect -i | grep client.*MPK | cut -f2 -d" ")0
 
 root=$PWD
 cd $root/ext/Orac/Core/orac
 puredata main.pd ../../../../modules/control/knobs.pd &
 sleep 1
-aconnect $akai:0 128:0
-aconnect 128:1 $akai:0
+aconnect $akai 128:0
+aconnect 128:1 $akai
 cd $root/ext/MEC/build/release/bin
 ./mec-app ../../../../Orac/macOS/MEC/osckontrol.json
